002638d90945051a1ec5e32f1bac8031106bdac5
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-26 16:29:45 +0000

    Create Post Schema

---
+++ models/Post.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const opts = {
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true },
+};
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema(
+  {
+    text: { type: String, required: true, maxLength: 1000 },
+    creator: { type: Schema.Types.ObjectId, required: true },
+    imageURLs: [{ type: String }],
+    datePosted: { type: Date, required: true },
+  },
+  opts
+);
+
+module.exports = mongoose.model("Post", PostSchema);
