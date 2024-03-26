242afda7efc6eb1f317f5f7192e870b65df7f270
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-26 16:45:22 +0000

    Create CommentSchema And Add UserRef To PostSchema

---
+++ models/Comment.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const opts = {
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true },
+};
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  text: { type: String, required: true, maxLength: 1000 },
+  author: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+  post: { type: Schema.Types.ObjectId, required: true, ref: "Post" },
+  imageUrl: { type: String },
+  dateCommented: { type: Date, required: true },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
--- models/Post.js
+++ models/Post.js
@@ -10,7 +10,7 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema(
   {
     text: { type: String, required: true, maxLength: 1000 },
-    creator: { type: Schema.Types.ObjectId, required: true },
+    creator: { type: Schema.Types.ObjectId, required: true, ref: "User" },
     imageURLs: [{ type: String }],
     datePosted: { type: Date, required: true },
   },
