18db88f1bf692447af4d3de48778d7e7b5254b7d
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-26 19:44:39 +0000

    Create PassportConfig And TestConfig File

---
+++ config/passport.js
@@ -0,0 +1,34 @@
+const bcrypt = require("bcryptjs");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/User");
+
+function initializePassport(passport) {
+  passport.use(
+    new LocalStrategy(async (username, password) => {
+      try {
+        const user = await User.findOne({ username: username });
+        if (!user) return done(null, false, { message: "No user" });
+
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) return done(null, false, { message: "Incorrect password" });
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    })
+  );
+
+  passport.serializeUser((user, done) => done(null, user.id));
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+}
+
+module.exports = initializePassport;
