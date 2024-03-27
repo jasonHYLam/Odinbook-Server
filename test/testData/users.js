e816911879c5626cdd238e673ac5c2180f428111
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-27 16:20:27 +0000

    Partially Establish Single User Data

--- test/testData/users.js
+++ test/testData/users.js
@@ -1,3 +1,11 @@
-const users = [{}];
+const { userIds } = require("./ids");
+
+const users = [
+  {
+    _id: userIds[0],
+    username: "User0",
+    password: "Abc123",
+  },
+];
 
 module.exports = users;
