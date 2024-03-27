1c732268ca8b5ec364b9c09d2b45d396bc8487b0
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-27 15:39:38 +0000

    Add More Setup To TestApp

--- test/testConfig/testApp.js
+++ test/testConfig/testApp.js
@@ -3,7 +3,30 @@ const session = require("express-session");
 const initializePassport = require("../../config/passport");
 const passport = require("passport");
 require("dotenv").config();
-// require("../../config/mongo");
 const indexRouter = require("../../routes/index");
 
+initializeMongoServer();
+initializePassport(passport);
+
 const app = express();
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: process.env.MODE === "prod",
+      secure: process.env.MODE === "prod",
+      sameSite: process.env.MODE === "prod" ? "none" : "lax",
+    },
+  })
+);
+
+app.use(passport.session());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+
+module.exports = app;
