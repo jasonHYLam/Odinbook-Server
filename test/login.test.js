5d6202eb7c1ea3121c38466af066cc00d90de74d
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-27 15:57:50 +0000

    Create Empty TestData Files

--- test/login.test.js
+++ test/login.test.js
@@ -1,2 +1,9 @@
 // test setup
+const {
+  initializeMongoServer,
+  closeMongoServer,
+} = require("./testConfig/mongo");
 const app = require("./testConfig/testApp");
+
+beforeAll(async () => await initializeMongoServer());
+afterAll(async () => await closeMongoServer());
