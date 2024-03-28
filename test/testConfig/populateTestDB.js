a83f52e9f2d61ac2cd692b1e5dccd29197a09ce7
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-28 12:10:46 +0000

    Refactor Adding Users In PopulateTestDB
    This now uses insertMany which is a faster operation than saving each
    created User document.

--- test/testConfig/populateTestDB.js
+++ test/testConfig/populateTestDB.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
 const Comment = require("../../models/Comment");
 const Post = require("../../models/Post");
@@ -5,19 +6,15 @@ const Post = require("../../models/Post");
 const users = require("../testData/users");
 
 async function createUserDoc(user) {
-  const newUser = new User({
-    username: user.username,
-    password: user.password,
-  });
-  await newUser.save();
+  const newUser = new User(user);
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+  newUser.password = hashedPassword;
 }
 
-function setUpUserDocs() {
-  users.map(async (user) => await createUserDoc(user));
-}
+const allTestUsers = users.map(async (user) => await createUserDoc(user));
 
 async function populateTestDB() {
-  await Promise.all([setUpUserDocs]);
+  await Promise.all([User.insertMany(allTestUsers)]);
 }
 
 module.exports = populateTestDB;
