e0514ae15417dc1b7f5da4c034072776136524fc
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-28 14:29:54 +0000

    Create Signup Function
    This contains validation for length. If no errors, then a new user will
    be attempted to be created. This requires bcryptjs to hash the password;
    error handling is added to account for potential errors.

--- controller/authController.js
+++ controller/authController.js
@@ -1,1 +1,42 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const he = require("he");
+
+const User = require("../models/User");
+
+const signup = [
+  body("username")
+    .trim()
+    .isLength({ min: 5, max: 20 })
+    .withMessage("must be between 5-20 characters")
+    .escape(),
+  body("password")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("must be at least 5 characters")
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(result);
+      return res.status(400).send({ errors: errors.array() });
+    } else {
+      escapedUsername = he.decode(req.body.username);
+      escapedPassword = he.decode(req.body.username);
+
+      try {
+        const hashedPassword = await bcrypt.hash(escapedPassword, 10);
+        const newUser = new User({
+          username: escapedUsername,
+          password: hashedPassword,
+        });
+        await newUser.save();
+        res.end();
+      } catch (err) {
+        if (err) next(err);
+      }
+    }
+  }),
+];
--- test/login.test.js
+++ test/login.test.js
@@ -24,7 +24,14 @@ const loginData = {
 };
 
 describe("login tests", () => {
-  describe("login", () => {
+  describe("signup", () => {
+    test("successful sign up", async () => {
+      const signUpData = { username: "newUser", password: "Abc123" };
+      const signupResponse = await agent.post("/signup").send(signUpData);
+      expect(signupResponse.status).toBe(200);
+    });
+  });
+  describe.skip("login", () => {
     test("successful login (valid credentials)", async () => {
       const loginResponse = await agent.post("/login").send(loginData);
       expect(loginResponse.status).toBe(200);
