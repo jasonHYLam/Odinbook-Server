a8a1941e1ec4f68fea93a906fe980508402d691e
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-29 09:16:40 +0000

    Create View_Personal_Profile Controller Callback

---
+++ controller/userController.js
@@ -0,0 +1,25 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/User");
+
+// view profile
+exports.view_personal_profile = asyncHandler(async (req, res, next) => {
+  const matchingUser = await User.findById(req.user.id);
+  console.log("checking matchingUser");
+  console.log(matchingUser);
+  // mm how do i get their posts?
+  const matchingUserPosts = await Post.find({ creator: req.user.id });
+  console.log("checking matchingUser");
+  console.log(matchingUserPosts);
+});
+
+exports.view_profile = asyncHandler(async (req, res, next) => {});
+// view feed
+// view particular post
+// view followers list
+// view folllowing list
+// follow account
+// remove follower
+// remove following
+// change username, password, profilePic
+//
+// exports.
