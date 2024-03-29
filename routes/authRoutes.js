a44aee5738ff0ea1ce0dba99b7ae18db453127e2
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-29 07:01:22 +0000

    Add AuthChecks To AuthRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controller/authController");
 
-router.post("/signup", authController.signup);
-router.post("/login", authController.login);
-router.delete("/logout", authController.logout);
+router.post(
+  "/signup",
+  authController.allowLoginOrSignup,
+  authController.signup
+);
+router.post("/login", authController.allowLoginOrSignup, authController.login);
+router.delete("/logout", authController.isAuthenticated, authController.logout);
 
 module.exports = router;
