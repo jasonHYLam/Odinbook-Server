ca7897d82f2a7c59811be020d10a437865826eca
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-29 06:45:44 +0000

    Change Logout Route Method From Get To Delete

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,6 @@ const authController = require("../controller/authController");
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
-router.post("/logout", authController.logout);
+router.delete("/logout", authController.logout);
 
 module.exports = router;
