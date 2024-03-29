7ee322aa0907bbeaef8cb89f439fcf8bf1d51a7c
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-29 06:44:39 +0000

    Create Logout Route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require("../controller/authController");
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.post("/logout", authController.logout);
 
 module.exports = router;
