65ff34a25970ceb48439ea3870c66b39ed90ec30
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-29 16:13:55 +0000

    Create Route For Change_Username

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,6 +8,6 @@ router.get("/", function (req, res, next) {
 });
 
 router.get("/view_personal_profile", userController.view_personal_profile);
-// router.post("/change_username", userController.);
+router.post("/change_username", userController.changeUsername);
 
 module.exports = router;
