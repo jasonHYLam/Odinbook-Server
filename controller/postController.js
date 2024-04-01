edbdb01208623e120930510693ddb860883990f2
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-01 22:24:15 +0100

    Make GetPost Controller Function
    This sends the post (populated with creator and likedByUsers) and
    comments (populated with author)

--- controller/postController.js
+++ controller/postController.js
@@ -4,7 +4,15 @@ const he = require("he");
 const User = require("../models/User");
 const Post = require("../models/Post");
 
-exports.view_post = asyncHandler(async (req, res, next) => {});
+exports.getPost = asyncHandler(async (req, res, next) => {
+  const { postID } = req.params;
+  const post = await Post.findById(postID)
+    .populate("creator")
+    .populate("likedBy");
+  const comments = await Comment.find({ post: postID }).populate("author");
+
+  res.status(201).send({ post, comments });
+});
 
 exports.view_all_posts = asyncHandler(async (req, res, next) => {});
 
