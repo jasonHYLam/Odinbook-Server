9ff0facd8ef66f5960f43dc08419ae3c682c9f22
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-02 16:08:33 +0100

    Put All_Posts Route Above Dynamic :postID Route
    This is to prevent the dynamic route from matching with the all_posts
    endpoint.

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controller/postController");
 
-router.get("/:postID", postController.getPost);
 router.get("/all_posts", postController.getAllPosts);
+router.get("/:postID", postController.getPost);
 
 module.exports = router;
