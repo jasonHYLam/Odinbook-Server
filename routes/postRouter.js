3b1ae9395f5e6dfa6bd9a6d1dde078377f319f3a
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-03 16:55:48 +0100

    Create Post Route For CreatePost Function

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const postController = require("../controller/postController");
 
 router.get("/all_posts", postController.getAllPosts);
+router.post("/create_post", postController.createPost);
+
 router.get("/:postID", postController.getPost);
 
 module.exports = router;
