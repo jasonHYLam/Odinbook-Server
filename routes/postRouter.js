27405c9e2561d366303d2c448792a3c7ffc08f1b
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-04 17:49:40 +0100

    Create Route For UnlikePost

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -7,6 +7,7 @@ router.post("/create_post", postController.createPost);
 router.post("/create_post_with_image", postController.createPostWithImage);
 router.put("/:postID/edit", postController.editPost);
 router.put("/:postID/like", postController.likePost);
+router.put("/:postID/unlike", postController.unlikePost);
 
 router.get("/:postID", postController.getPost);
 
