b1cef2d69b53efa23493b654d9a1717206202d9a
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-04 15:59:54 +0100

    Create Route For DeleteComment

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -4,5 +4,6 @@ const commentController = require("../controller/commentController");
 
 router.post("/:postID/comment", commentController.writeComment);
 router.put("/:postID/:commentID/edit", commentController.editComment);
+router.delete("/:postID/:commentID/delete", commentController.deleteComment);
 
 module.exports = router;
