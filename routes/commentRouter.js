018a89432a8538775ca4ede30b42f21caf071506
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-04 13:04:30 +0100

    Create EditComment Route

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const commentController = require("../controller/commentController");
 
 router.post("/:postID/comment", commentController.writeComment);
+router.put("/:postID/:commentID/edit", commentController.writeComment);
 
 module.exports = router;
