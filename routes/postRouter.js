3b5272c76afdab6c4a12c89e062f7d6dce36a7ba
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-04 18:13:59 +0100

    Write DeletePost Route

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -8,6 +8,7 @@ router.post("/create_post_with_image", postController.createPostWithImage);
 router.put("/:postID/edit", postController.editPost);
 router.put("/:postID/like", postController.likePost);
 router.put("/:postID/unlike", postController.unlikePost);
+router.delete("/:postID/delete", postController.deletePost);
 
 router.get("/:postID", postController.getPost);
 
