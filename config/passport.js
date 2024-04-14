507d23638fa2e1d1530156cdc5bfb767401ab447
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-14 14:21:39 +0100

    Remove Comments From Passport

--- config/passport.js
+++ config/passport.js
@@ -8,24 +8,13 @@ function initializePassport(passport) {
       try {
         const user = await User.findOne({ username: username });
         if (!user) {
-          console.log("no user");
           return done(null, false, { message: "No user" });
         }
-        console.log("user seems to exist");
-
-        console.log("password");
-        console.log(password);
-        console.log("user.password");
-        console.log(user.password);
         const match = await bcrypt.compare(password, user.password);
-        console.log("checking match");
-        console.log(match);
 
         if (!match) {
-          console.log("incorrect password");
           return done(null, false, { message: "Incorrect password" });
         }
-        console.log("all good");
 
         return done(null, user);
       } catch (err) {
