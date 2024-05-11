2cdb9f67e1a0502a9ee51ee2794e2c8f9496435a
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-05-11 22:15:25 +0100

    Make route for toggleBookmarkPost

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -10,6 +10,7 @@ router.post("/create_post_with_image", postController.createPostWithImage);
 router.put("/:postID/edit", postController.editPost);
 router.put("/:postID/like", postController.likePost);
 router.put("/:postID/unlike", postController.unlikePost);
+router.put("/:postId/toggle_bookmark", postController.toggleBookmarkPost);
 router.delete("/:postID/delete", postController.deletePost);
 
 router.get("/:postID", isAuthenticated, postController.getPost);
