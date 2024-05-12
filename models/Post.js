1017ec51430a8c68db6abd7c787ee777cc89006e
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-05-12 20:02:17 +0100

    Add thumbnail property to Post schema
    This will be used for the feed and other
    collections of posts.

--- models/Post.js
+++ models/Post.js
@@ -14,6 +14,7 @@ const PostSchema = new Schema(
     text: { type: String, required: true, maxLength: 1000 },
     creator: { type: Schema.Types.ObjectId, required: true, ref: "User" },
     imageURL: { type: String },
+    thumbnailImageURL: { type: String },
     datePosted: { type: Date, required: true },
     likedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
     bookmarkedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
